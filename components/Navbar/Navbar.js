e7f631f6cdaeeb487bf15ba8f7f59509f46f2136
rishav4101 <rajkumarrishav4101@gmail.com>
2021-03-15 17:34:08 +0530

    Navbar change

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,7 +3,56 @@ import React from "react";
 export default function Navbar() {
   return (
     <header>
-      <div className="header-area ">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid">
+        <div className="logo-img">
+                  <a href="#" style={{ display: "flex"}}>
+                   <img src="img/aarohan-logo.png" style={{
+                      height: "50px",
+                      width: "50px",
+                      borderRadius: "50%",
+                      float: "left",
+                    }} />&nbsp;&nbsp;<img src="img/logo.png" style={{ float: "left", }} alt />
+                  </a>
+                </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{textAlign:"right"}}>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ml-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Discord
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="/schedule"
+                >
+                  Schedule
+                </a>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+
+      {/* <div className="header-area ">
         <div id="sticky-header" className="main-header-area ">
           <div className="container-fluid p-0">
             <div className="row align-items-center justify-content-between no-gutters">
@@ -51,7 +100,7 @@ export default function Navbar() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </header>
   );
 }
